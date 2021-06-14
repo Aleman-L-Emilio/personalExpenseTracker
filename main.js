02c0cbdfb57d9c99342e6806780ecb38ac048e65
elewis804 <60186516+elewis804@users.noreply.github.com>
2021-06-14 15:32:10 -0400

    moved the function to edit expenses.txt to the index.html file so that it can actually be called. also attempted to make it so the user can't spam blank inputs (partially works)

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ const { app, BrowserWindow } = require('electron');
 const electron = require('electron');
 const path = require('path');
 const url = require('url');
-const fs = require('fs');
 const prompt = require('electron-prompt');
 
 var firstAccount;
 let win;
 
 app.on('ready', () => {
-    const electronScreen = electron.screen;
-    const { width, height } = electronScreen.getPrimaryDisplay().workAreaSize
     win = new BrowserWindow({
-        width: 1400,
+        width: 1450,
         height: 800,
         useContentSize: true,
         webPreferences: {
@@ -43,19 +40,8 @@ app.on('ready', () => {
 });
 
 app.on('window-all-closed', () => {
-    // app won't fully close if it's on mac so this line is here just to make sure it doesn't get left open accidentally
+    // app won't fully close if it's on mac so this line is here just to make sure it doesn't get left open in the background accidentally
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
-
-function addExpense(date, payee, category, payment) {
-    // TODO: Make sure payee and category are strings/words, and make sure that payment is a number
-    fs.appendFile('app/expenses.txt', date + ';' +
-        payee + ';' + category + ';' + payment + '\n ',
-        function(err) {
-            if (err) throw err;
-            console.log('Added payment information');
-        }
-    );
-}
